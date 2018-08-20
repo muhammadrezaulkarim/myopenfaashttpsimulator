796d52afd183207d14a770f08c0f2c36cc2b54ad
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-08-20 00:16:52 -0600

    Update handler.py

--- function/handler.py
+++ function/handler.py
@@ -1,7 +1,23 @@
+import json
+from flask import Response
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
+    #convert the json representation into a python object
+    json_req = json.loads(req)
+    
+    # extract key and value
+    result = {"key": json_req["key"], "value": json_req["value"]}
+    
+    # serialize to a JSON formatted string 
+    str_rep = json.dumps(result)
+    log.debug('Received Message: ' + str_rep)
+    resp = Response(str_rep, status=200, mimetype='application/json')
 
-    return req
+    return resp
