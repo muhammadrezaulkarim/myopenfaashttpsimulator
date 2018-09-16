7f67678197b48ad9904203abbc93adacba2ca4bc
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-09-15 18:02:52 -0600

    Update handler.py

--- function/handler.py
+++ function/handler.py
@@ -1,7 +1,18 @@
+import json
+from flask import Response
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
+    
+    #log.debug('Data: ' + received_data)
+    log.debug('Dict: ' + str(req.data))
 
-    return req
+    return 'Received Message: ' + str(req.data)
+ 
